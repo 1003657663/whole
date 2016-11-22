03dc4a8f98e1bb841554141e8da49f4465088e5b
chao song <chao song>
2016-11-22 18:17:12 +0800

    sdj

--- app/html/handleIndexData.js
+++ app/html/handleIndexData.js
@@ -8,37 +8,55 @@ let handChildHtml = require('./handleChildHtml');
 
 module.exports = function (data, file, htmlDest) {
     //data是来自主html文件的data
+
+    var defaultEl = {
+        script:{srcTag:"src"},
+        link:{srcTag:"href"},
+        style:{}
+    };
+
+    let wholeins;
     let $dom = $.load(data,{decodeEntities:false});
-    let wholeScript = $dom('script[type=whole]');
-    let wholeLink = $dom('link[type=whole]');
-    let locationStyle = $dom('style[type=whole]');
-    let wholeins = $dom('wholein');
 
-    let locationScript = wholeScript.filter(':not([src])');
-    let locationLink = wholeLink.filter(':not([href])');
+    $dom("*").filter(function () {
+        var $this = $(this);
+        if($this.is("wholein")){
+            if($this.is(":not([src])")){
+                console.error("主页面:" + file + "中包含的wholein标签必须含有src属性，请删除不含src属性的wholein标签");
+                process.exit();
+            }
+            if(wholeins){
+                wholeins.add($this);
+            }else{
+                wholeins = $this;
+            }
+        }else{
+            for(let i in defaultEl){
+                let elementInfo = defaultEl[i];
+                if($this.is(i)){
+                    if($this.is("[type=whole]")){
+                        if($this.is("["+elementInfo.srcTag+"]")){
+                            if(elementInfo.src){
+                                elementInfo.src.add($this);
+                            }else {
+                                elementInfo.src = $this;
+                            }
+                        }else{
+                            if(elementInfo.location){
+                                //第二个错误
+                                console.error("主页面:" + file + "中包含多个定位"+i+"标签，规定只能有一个");
+                                process.exit();
+                            }else {
+                                elementInfo.location = $this;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
 
-
-    if (wholeins.filter(':not([src])').length > 0) {
-        console.error("主页面:" + file + "中包含的wholein标签必须含有src属性，请删除不含src属性的wholein标签");
-        process.exit();
-    }
-    if (locationScript.length > 1) {
-        console.error("主页面:" + file + "中包含多个定位script标签，规定只能有一个");
-        process.exit();
-    }
-    if (locationLink.length > 1) {
-        console.error("主页面:" + file + "中包含多个定位link标签，规定只能有一个");
-        process.exit();
-    }
-    if (locationStyle.length > 1) {
-        console.error("主页面:" + file + "中包含多个定位style标签，规定只能有一个");
-        process.exit();
-    }
-
-
-    let srcScript = wholeScript.filter('script[src]');
-    let srcLink = wholeLink.filter('link[href]');
-
+    console.log()
     let length = wholeins.length;
     for (let i = 0; i < length; i++) {
         handChildHtml(wholeins.eq(i), file,
